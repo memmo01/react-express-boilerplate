f95977d917ae415372cce9dbef6081be807da34c
michael emmons <memmons18@gmail.com>
2018-08-28 12:13:57 -0400

    added boilerplate for using react with express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+const Port = 5000;
+
+app.listen(Port, () => console.log(`Server started on port ${Port}`));
